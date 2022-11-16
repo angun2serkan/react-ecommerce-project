b33d6858e72fa3d03807602e1272b2d0ba383842
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-16 20:56:54 +0300

    props are added for product informations

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,24 +10,69 @@ function Home() {
             <div className={classes.home__container}>
                 <img src='https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140603_960_720.jpg' alt='' className={classes.home__image}/>
                 <div className={classes.home__row}>
-                  <Product/>
-                  <Product/>
+                  <Product 
+                  id="15345856556"
+                  title=", as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lo"
+                  price={11.96}
+                  rating={5}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
+                  <Product
+                  id="0403196"
+                  title="from a Lorem Ipsum passage, and going through the cites of the word in classical"
+                  price={23}
+                  rating={4}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
                 </div>
 
                 <div className={classes.home__row}>
-                  <Product/>
-                  <Product/>
-                  <Product/>
+                <Product
+                  id="0403196"
+                  title="from a Lorem Ipsum passage, and going through the cites of the word in classical"
+                  price={23}
+                  rating={4}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
+                  <Product 
+                  id="15345856556"
+                  title=", as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lo"
+                  price={11.96}
+                  rating={5}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
+                  <Product 
+                  id="15345856556"
+                  title=", as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lo"
+                  price={11.96}
+                  rating={5}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
                 </div>
 
                 <div className={classes.home__row}>
-                  <Product/>
-                  <Product/>
-                  <Product/>
+                <Product 
+                  id="15345856556"
+                  title=", as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lo"
+                  price={11.96}
+                  rating={5}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
+                  <Product
+                  id="0403196"
+                  title="from a Lorem Ipsum passage, and going through the cites of the word in classical"
+                  price={23}
+                  rating={4}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
+                  <Product
+                  id="0403196"
+                  title="from a Lorem Ipsum passage, and going through the cites of the word in classical"
+                  price={23}
+                  rating={4}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
                 </div>
 
                 <div className={classes.home__row}>
-                  <Product/>
+                <Product
+                  id="0403196"
+                  title="from a Lorem Ipsum passage, and going through the cites of the word in classical"
+                  price={23}
+                  rating={4}
+                  image="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg"/>
                 </div>
             </div>
         </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import classes from './Product.module.css';
 
-function Product() {
+function Product(props) {
   return (
     <div>
         <div className={classes.product}>
             <div className={classes.product__info}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut nihil fugit velit obcaecati nisi quae voluptas eius provident alias, tenetur consectetur, omnis repellat incidunt?</p>
+                <p>{props.title}</p>
                 <p className={classes.product__price}>
-                  <small></small>
-                  <strong></strong>
+                  <small>$</small>
+                  <strong>{props.price}</strong>
                 </p>
-                <div className={classes.product__rating}>⭐⭐⭐</div>
+                <div className={classes.product__rating}>
+                  {Array(props.rating)
+                  .fill()
+                  .map((_, i) => (
+                    <p>⭐</p>
+                  ))}
+                </div>
             </div>
 
-            <img src="https://i.pinimg.com/originals/9b/07/a9/9b07a9902e0a62b91c80adfc0e50eb68.jpg" alt="product-image" />
+            <img src={props.image} alt="product-image" />
 
             <button>Add to Basket</button>
             
