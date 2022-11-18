f491d5e344937d673fb5381d78b580b4e42ec914
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-18 15:32:31 +0300

    remove from basket

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -30,7 +30,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                         <p>⭐</p>
                     ))}
                 </div>
-                <button onClick={removeFromBasket}>Remove from Basket</button>
+                <button className={classes.checkoutProduct__button} onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
--- src/reducer.js
+++ src/reducer.js
@@ -13,12 +13,35 @@ export const getBasketTotal = (basket) => {
 }
 
 const reducer = (state, action) => {
-    switch(action.type){
+    switch(action.type) {
         case "ADD_TO_BASKET":
-            return{
+            return {
                 ...state,
                 basket: [...state.basket, action.item],
             }
+        
+        case "REMOVE_FROM_BASKET":
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index>=0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product(id: ${action.id}) as its not in the basket!`
+                )
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
+        default:
+            return state;
     }
 }
 
